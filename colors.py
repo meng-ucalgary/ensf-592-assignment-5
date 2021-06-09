f9b0dbdfb0ccda50f713452e94cf4b63a31a9ec6
Bhavyai Gupta <zbhavyai@gmail.com>
2021-06-09 16:47:55 +0530

    Added screenshots and supporting source code

---
+++ colors.py
@@ -0,0 +1,10 @@
+# colors.py
+# Bhavyai Gupta
+#
+# Defines variables to hold the ANSI codes for coloring text
+
+red = "\033[0;91m"
+green = "\033[0;92m"
+yellow = "\033[0;93m"
+cyan = "\033[0;96m"
+reset = "\033[0m"
